75af804893f2365845695e65e40cdd1cf2c5eb47
S Fujimoto <unicoshun@gmail.com>
2018-03-31 16:59:58 +0900

    update version no

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="PyPPTX",
-    version="0.1.0",
+    version="0.1.5",
     author="sf",
     author_email="s-fujimoto@seig-boys.jp",
     packages=["src", "data", "output", "ppt"],
